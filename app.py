8f31d710e4598fb5d268e88c593ebdcc22decf40
vinayak-panda <vinayakpanda1810@gmail.com>
2026-03-30 20:20:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -762,6 +762,6 @@ def init_db():
 # Initialize DB on startup (works with gunicorn too)
 init_db()
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
